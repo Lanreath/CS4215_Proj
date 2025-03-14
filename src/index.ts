a1b461183de535c8c60bd5531dd452f6f28a3a47
Lanreath <tyfei00@gmail.com>
2025-03-14 11:12:25 +0800

    Init

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { initialise } from "conductor/dist/conductor/runner/util/";
 import { SimpleLangEvaluator } from "./SimpleLangEvaluator";
 
+// Initialise the runner plugin and conduit
 const {runnerPlugin, conduit} = initialise(SimpleLangEvaluator);
